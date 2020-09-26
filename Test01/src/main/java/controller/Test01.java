8adec71f459953b417ccdfc8b4fa348f83d5ddea
addspell <49441750+addspell@users.noreply.github.com>
2020-09-26 19:37:53 +0800

    Update Test01.java

--- Test01/src/main/java/controller/Test01.java
+++ Test01/src/main/java/controller/Test01.java
@@ -4,6 +4,6 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("第一次传输");
         System.out.println("第二次提交");
-        System.out.println("测试拉取");
+        System.out.println("第三次");
     }
 }
