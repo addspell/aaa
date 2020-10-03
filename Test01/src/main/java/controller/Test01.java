781f906370340590996b568c1cc1e027c3dbc2a8
CHL <CHL@gamil.com>
2020-10-03 11:16:42 +0800

    主分支，测试asList方法

--- Test01/src/main/java/controller/Test01.java
+++ Test01/src/main/java/controller/Test01.java
@@ -1,7 +1,17 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("第一次传输");
+        Integer[] myArray = {1, 2, 3};
+        List myList = Arrays.asList(myArray);
+        ArrayList list = new ArrayList<>(myList);
+        list.add(4);
+        System.out.println(list);
+
     }
 }
