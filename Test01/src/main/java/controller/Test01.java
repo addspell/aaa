11a75fb5b4da9443c310775771841b66d1bf54d3
CHL <CHL@gamil.com>
2020-10-20 09:17:48 +0800

    dev,修改Test01.java，测试冲突

--- Test01/src/main/java/controller/Test01.java
+++ Test01/src/main/java/controller/Test01.java
@@ -2,6 +2,7 @@ package controller;
 
 public class Test01 {
     public static void main(String[] args) {
+        System.out.println("测试冲突");
         int a=10;
         int b=20;
         if(b>a){
