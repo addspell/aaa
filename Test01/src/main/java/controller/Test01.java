6ca092c5a69726d607a91bc81a2288a1ac76468e
CHL <CHL@gamil.com>
2020-10-20 09:08:39 +0800

    dev,修改Test01.java

--- Test01/src/main/java/controller/Test01.java
+++ Test01/src/main/java/controller/Test01.java
@@ -2,10 +2,11 @@ package controller;
 
 public class Test01 {
     public static void main(String[] args) {
-        System.out.println("第一次传输");
-        System.out.println("第二次提交");
-        System.out.println("第三次");
-        System.out.println("第四次");
-        System.out.println("第五次");
+        int a=10;
+        int b=20;
+        if(b>a){
+            a=b;
+        }
+        System.out.println(a,b);
     }
 }
