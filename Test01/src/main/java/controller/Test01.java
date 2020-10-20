859295a291ba1f955a608cbd75fa0c85344d2cf1
CHL <CHL@gamil.com>
2020-10-20 09:39:10 +0800

    dev,修改Test01.java，测试冲突3

--- Test01/src/main/java/controller/Test01.java
+++ Test01/src/main/java/controller/Test01.java
@@ -8,15 +8,7 @@ import java.util.List;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println(a,b);
-        HashMap<Object, Object> HashMap = new HashMap<>();
-        
-        
-        int a=10;
-        int b=10;
-        int c=a+b;
-        int d=b+c;
-        int e=d+b;
-        int g=a+e;
+        HashMap hashMap = new HashMap();
 
     }
 }
