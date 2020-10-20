57fc25791d12b86dffef3cb6b2edcb2c68e1dff7
addspell <49441750+addspell@users.noreply.github.com>
2020-10-20 09:35:18 +0800

    Update Test01.java

--- Test01/src/main/java/controller/Test01.java
+++ Test01/src/main/java/controller/Test01.java
@@ -9,11 +9,12 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println(a,b);
         HashMap<Object, Object> HashMap = new HashMap<>();
+        
+        
         int a=10;
         int b=10;
-        int c=10;
-        int d=10;
-        int e=10;
+        int c=a+b;
+        int d=b+c;
 
 
     }
