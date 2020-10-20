f94c54e6a8fc388e5b99b24fc03a693e1da0a6bf
CHL <CHL@gamil.com>
2020-10-20 09:02:42 +0800

    主分支，修改Test01.java,增加map数据
    (cherry picked from commit d88bf1bae5ea74c0f53b34f566c6620cf5343f25)

--- Test01/src/main/java/controller/Test01.java
+++ Test01/src/main/java/controller/Test01.java
@@ -1,5 +1,10 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("测试冲突");
@@ -9,5 +14,10 @@ public class Test01 {
             a=b;
         }
         System.out.println(a,b);
+        HashMap<Object, Object> HashMap = new HashMap<>();
+        HashMap.put("1","测试1");
+        HashMap.put("2","测试2");
+        HashMap.put("2","测试2");
+
     }
 }
