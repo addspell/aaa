e6fc36eb1d9dbe4a0eca64475b7b51ac707dc230
CHL <CHL@gamil.com>
2020-10-20 09:28:38 +0800

    dev,修改Test01.java，测试冲突

--- Test01/src/main/java/controller/Test01.java
+++ Test01/src/main/java/controller/Test01.java
@@ -7,12 +7,6 @@ import java.util.List;
 
 public class Test01 {
     public static void main(String[] args) {
-        System.out.println("测试冲突");
-        int a=10;
-        int b=20;
-        if(b>a){
-            a=b;
-        }
         System.out.println(a,b);
         HashMap<Object, Object> HashMap = new HashMap<>();
         HashMap.put("1","测试1");
