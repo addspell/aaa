da39bf23173a0cec1ea4f848de27eea188182b15
addspell <49441750+addspell@users.noreply.github.com>
2020-10-20 09:03:42 +0800

    Update Test01.java

--- Test01/src/main/java/controller/Test01.java
+++ Test01/src/main/java/controller/Test01.java
@@ -8,9 +8,8 @@ import java.util.List;
 public class Test01 {
     public static void main(String[] args) {
         HashMap<Object, Object> HashMap = new HashMap<>();
-        HashMap.put("1","测试1");
-        HashMap.put("2","测试2");
-        HashMap.put("2","测试2");
+        int a=10;
+        int b=30;
 
     }
 }
