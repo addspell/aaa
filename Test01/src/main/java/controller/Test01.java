d7878d2b6e0a61e83b2cb91acc81b9dae3d83728
CHL <CHL@gamil.com>
2020-10-20 09:46:21 +0800

    dev,修改Test01.java，测试冲突4

--- Test01/src/main/java/controller/Test01.java
+++ Test01/src/main/java/controller/Test01.java
@@ -9,7 +9,9 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println(a,b);
         HashMap hashMap = new HashMap();
-
+        int a=10;
+        int b=20;
+        int c=a+b;
         System.out.println("第一次传输");
         System.out.println("生产环境第一次测试");
     }
