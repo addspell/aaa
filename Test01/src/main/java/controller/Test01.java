078c200722a14a05133340ce0d022cb8d02fd5a5
CHL <CHL@gamil.com>
2020-11-01 23:31:23 +0800

    我加了个方法

--- Test01/src/main/java/controller/Test01.java
+++ Test01/src/main/java/controller/Test01.java
@@ -6,12 +6,20 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Test01 {
+    public void test(){
+        HashMap<Object, Object> HashMap = new HashMap<>();
+        int a=10;
+        int b=a+10;
+        System.out.println("测试git");
+        System.out.println("开始测试git合并");
+        System.out.println("");
+    }
     public static void main(String[] args) {
         HashMap<Object, Object> HashMap = new HashMap<>();
         int a=10;
         int b=a+10;
         System.out.println("测试git");
         System.out.println("开始测试git合并");
-        
+        System.out.println("");
     }
 }
